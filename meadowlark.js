bf51a0e13f38979fc24ad955dd4daef260fe3a29
wu jian <wujian016@163.com>
2015-09-10 15:14:17 +0800

    add views

--- meadowlark.js
+++ meadowlark.js
@@ -1,25 +1,43 @@
 var express = require('express');
 var app = express();
-app.set('port',process.env.PORT || 3000);
+var handlebars = require('express3-handlebars').create({
+	defaultLayout: 'main'
+});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 
+app.set('port', process.env.PORT || 3000);
 
-app.get('/',function(req, res) {
-	res.type('text/plain');
-	res.send('Markdowlark Travel');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+	res.render('home');
 });
 
-app.get('/about',function(err, res) {
-	res.type('text/plain');
-	res.send('About meadowlark Travel');
+app.get('/about', function(err, res) {
+	var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+	res.render('about',{fortune:randomFortune});
 });
 
-app.use(function(req, res) {
-	res.type('text/plain');
+app.use(function(req, res, next) {
 	res.status(404);
-	res.send('404 - Not Found');
+	res.render('404');
 });
 
-app.listen(app.get('port'),function(){
+app.use(function(err, req, res, next) {
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
+});
+
+app.listen(app.get('port'), function() {
 	console.log('Express started on http://localhost:' +
 		app.get('port') + ';press Ctrl-C to terminate.');
 })
+
+var fortunes = [
+	"Couqure your fears or they will conquer you.",
+	"Rivers need springs.",
+	"Do not fear what you don't know.",
+	"Whenever possible, keep it simple."
+]
