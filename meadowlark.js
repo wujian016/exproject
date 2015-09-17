7d204448d534a4644dc35fb9b0d74a79e0101a87
wu jian <wujian016@163.com>
2015-09-17 21:56:22 +0800

    add template

--- meadowlark.js
+++ meadowlark.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var fortune = require('./lib/fortune.js');
 var handlebars = require('express3-handlebars').create({
-	defaultLayout: 'main'
+	defaultLayout: 'main',
+	helpers: {
+		section: function(name, options) {
+			if (!this._sections) this._sections = {};
+			this._sections[name] = options.fn(this);
+			return null;
+		}
+	}
 });
 var app = express();
 app.engine('handlebars', handlebars.engine);
@@ -17,13 +24,65 @@ app.use(function(req, res, next) {
 	next();
 });
 
+// mocked weather data
+function getWeatherData() {
+	return {
+		locations: [{
+			name: 'Portland',
+			forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+			iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+			weather: 'Overcast',
+			temp: '54.1 F (12.3 C)',
+		}, {
+			name: 'Bend',
+			forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+			iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+			weather: 'Partly Cloudy',
+			temp: '55.0 F (12.8 C)',
+		}, {
+			name: 'Manzanita',
+			forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+			iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+			weather: 'Light Rain',
+			temp: '55.0 F (12.8 C)',
+		}, ],
+	};
+}
+
+// middleware to add weather data to context
+app.use(function(req, res, next) {
+	if (!res.locals.partials) res.locals.partials = {};
+	res.locals.partials.weather = getWeatherData();
+	next();
+});
+
+// app.use(
+// 	function(req, res, next) {
+// 		res.locals = {
+// 			currency: {
+// 				name: "united stateds dollars",
+// 				abbrev: "USD",
+// 			},
+// 			tous: [{
+// 				name: "hood river",
+// 				price: '$99.95}'
+// 			}, {
+// 				name: 'Oregon cost',
+// 				price: '$159.95'
+// 			}, ],
+// 			specialsUrl: '/january-specials',
+// 			currencies: ['USD', 'GBP', 'BTC']
+// 		};
+// 		next();
+// 	});
+
 app.get('/', function(req, res) {
 	res.render('home');
 });
 
 app.get('/headers', function(req, res) {
-	res.set('Content-Type','text/plain');
-	var s='';
+	res.set('Content-Type', 'text/plain');
+	var s = '';
 	// for (var name in req.headers) s+=name+': ' + req.headers[name] + '\n';
 	// 	res.send(s);
 	console.log(req.ip);
@@ -36,6 +95,50 @@ app.get('/about', function(err, res) {
 	res.render('about', {
 		fortune: fortune.getFortune(),
 		pageTestScript: '/qa/tests-about.js'
+	});
+});
+
+app.get('/foo', function(req, res) {
+	res.render('foo', {
+		layout: 'microsite'
+	});
+});
+
+app.get('/jquerytest', function(err, res) {
+	res.render('jquerytest', {
+		fortune: fortune.getFortune(),
+		pageTestScript: '/qa/tests-about.js'
+	});
+});
+
+app.get('/test', function(err, res) {
+	res.render('test', {
+		currency: {
+			name: "united stateds dollars",
+			abbrev: "USD",
+		},
+		tours: [{
+			name: "hood river",
+			price: '$99.95'
+		}, {
+			name: 'Oregon cost',
+			price: '$159.95'
+		}, ],
+		specialsUrl: '/january-specials',
+		currencies: ['USD', 'GBP', 'BTC']
+	});
+});
+
+app.get('/nursery-rhyme', function(req, res) {
+	res.render('nursery-rhyme');
+});
+
+app.get('/data/nursery-rhyme', function(req, res) {
+	res.json({
+		animal: 'squirrel',
+		hodyPart: 'tail',
+		adjective: 'bushy',
+		noun: 'heck',
 	});
 });
 
@@ -54,4 +157,3 @@ app.listen(app.get('port'), function() {
 	console.log('Express started on http://localhost:' +
 		app.get('port') + ';press Ctrl-C to terminate.');
 });
-
